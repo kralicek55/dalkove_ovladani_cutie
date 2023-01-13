404d8afe5421cf96a5f906c0a8340be4469b40ab
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2023-01-13 19:27:06 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,8 @@ radio.onReceivedValue(function (name, value) {
         Y = value
     }
 })
+let rychlost_prave_kolo = 0
+let rychlost_leve_kolo = 0
 let Y = 0
 let X = 0
 radio.setGroup(90)
@@ -17,5 +19,23 @@ basic.showLeds(`
     . # . # .
     `)
 basic.forever(function () {
-    cuteBot.motors(Y + X, Y - X)
+    rychlost_leve_kolo = Y + X
+    rychlost_prave_kolo = Y - X
+    cuteBot.motors(rychlost_leve_kolo, rychlost_prave_kolo)
+    if (rychlost_leve_kolo > rychlost_prave_kolo) {
+        for (let index = 0; index < 4; index++) {
+            cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0xff8000)
+            basic.pause(250)
+            cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0x000000)
+            basic.pause(100)
+        }
+    }
+    if (rychlost_prave_kolo > rychlost_leve_kolo) {
+        for (let index = 0; index < 4; index++) {
+            cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff8000)
+            basic.pause(250)
+            cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x000000)
+            basic.pause(100)
+        }
+    }
 })
